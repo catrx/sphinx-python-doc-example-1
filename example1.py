101ef97595cb7f5233487eec4fb4f48812e0b6b9
senthilkumarm1901 <senthilkumar.m1901@gmail.com>
2019-08-23 11:59:50 +0530

    adding functions to document

---
+++ example1.py
@@ -0,0 +1,32 @@
+def func1(arg1, arg2):
+    """
+    This function, takes 2 arguments assigns the first argument with the second argument value
+
+    :param arg1: some value
+    :param arg2: another value
+
+    :return: arg1
+    # written in reStructurec Text doc string
+    """
+    arg1 = arg2
+    return arg1
+
+# def func2(arg1, arg2):
+# #     """
+# #     This function, takes 2 arguments assigns the first argument with the second argument value
+# #     Args:
+# #         arg1: some value
+# #         arg2: another value
+# #
+# #     Returns:
+# #         arg1
+# #     """
+# #     arg1 = arg2
+# #     return arg1
+
+def func2():
+    """
+    Function with no argument return none
+    :return: None
+    """
+    return None
